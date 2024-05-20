1d3a6dbc8bc02c5a1b619575ac4d43e79645af3d
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-05-20 20:55:06 +0430

    Creat person Entity

---
+++ src/main/java/IO/model/Person.java
@@ -0,0 +1,30 @@
+package IO.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName;
+
+    private int age;
+
+}
